3340b875de73554b4693e6ca901168fff7c7e7c5
michaltrznadel <11550231@wp.pl>
2023-05-17 16:43:06 +0200

    analiza skupień zmiana metody na spektralną

--- analiza_skupien/analiza_skupien.R
+++ analiza_skupien/analiza_skupien.R
@@ -1,16 +1,13 @@
+library(mlbench)
+spirale<-mlbench.spirals(240,1.5,0.03)
+data<-spirale$x
+klasy<-as.numeric(3)
+colornames <- c("red","blue" )
+data
+plot(data,col=colornames[klasy],ask=TRUE)
 
 
-# Wczytanie zbioru danych
-xx <- read.csv2("C:/statystykamichalAdam/analiza_skupien/Dane_zp_2007.csv", header=TRUE, fileEncoding="latin1")
-x <- as.matrix(xx[, 2:ncol(xx)])
-options(OutDec=",")
-
-
-
-library(clusterSim)
-sbe<-shapes.bulls.eye(numObjects=c(120,60))
-plot(sbe$data,col=rainbow(2)[sbe$clusters])
-
+#spectral method end
 windows()
 # nc - number_of_clusters
 min_nc=2
@@ -20,7 +17,7 @@ res[,1] <- min_nc:max_nc
 clusters <- NULL
 for (nc in min_nc:max_nc)
 {
-  cl2 <- speccl(sbe$data,nc,distance="sEuclidean",sigma="automatic",sigma.interval="default",mod.sample=0.75,R=10,iterations=3)
+  cl2 <- speccl(data,nc,distance="sEuclidean",sigma="automatic",sigma.interval="default",mod.sample=0.75,R=10,iterations=3)
   res[nc-min_nc+1,2] <- G1 <- index.G1(cl2$Ymatrix,cl2$clusters,centrotypes="centroids")
   clusters <- rbind(clusters, cl2$clusters)
 }
@@ -31,8 +28,9 @@ print(clusters[which.max(res[,2]),])
 plot(res, type="p", pch=0, xlab="Number of clusters", ylab="G1", xaxt="n")
 axis(1, c(min_nc:max_nc))
 print(cl2$sigma)
-cRand<-comparing.Partitions(as.vector(clusters[which.max(res[,2]),]),as.vector(sbe$clusters),type="crand")
+cRand<-comparing.Partitions(as.vector(clusters[which.max(res[,2]),]),as.vector(klasy),type="crand")
 print(cRand)
-windows()
+
 kl=clusters[which.max(res[,2]),]
-plot(sbe$data,col=rainbow(2)[kl])
+plot(data, col=rainbow(2)[kl])
+#spectral method end
